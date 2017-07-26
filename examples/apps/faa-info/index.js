42ec6a29ac4e9c9f82841a867eb25b93183608f4
frustratedidiot123 <frustratedidiot123@users.noreply.github.com>
2017-07-26 18:04:26 -0400

    Update index.js

--- examples/apps/faa-info/index.js
+++ examples/apps/faa-info/index.js
@@ -6,8 +6,8 @@ var app = new Alexa.app('airportinfo');
 var FAADataHelper = require('./faa_data_helper');
 
 app.launch(function(req, res) {
-  var prompt = 'For delay information, tell me an Aiport code.';
-  var reprompt = 'Tell me an Aiport code';
+  var prompt = 'For delay information, give me an Airport code.';
+  var reprompt = 'Please tell your Airport code';
   res.say(prompt).reprompt(reprompt).shouldEndSession(false);
 });
 
@@ -22,7 +22,7 @@ app.intent('airportinfo', {
     var airportCode = req.slot('AIRPORTCODE');
     var reprompt = 'Tell me an airport code to get delay information.';
     if (_.isEmpty(airportCode)) {
-      var prompt = 'I didn\'t hear an airport code. Tell me an airport code.';
+      var prompt = 'I didn\'t hear an airport code. Please give me an airport code.';
       res.say(prompt).reprompt(reprompt).shouldEndSession(false);
       return true;
     } else {
@@ -34,7 +34,7 @@ app.intent('airportinfo', {
         res.say(faaHelper.formatAirportStatus(airportStatus)).send();
       }).catch(function(err) {
         console.log('err.statusCode: ',err.statusCode);
-        var prompt = 'I don\'t have data for an airport code of ' + airportCode;
+        var prompt = 'I don\'t have data for any ' + airportCode;
         res.say(prompt).reprompt(reprompt).shouldEndSession(false).send();
       });
       //return false;
